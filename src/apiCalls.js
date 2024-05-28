110073665b5d3ae826dd82a77e2a2ec36d7ee386
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-05-28 16:24:00 +0100

    bug fix 2

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // import { userThemeContext } from "./ThemeProvider";
 
 // export const baseUrl= "http://51.20.63.132";
-export const baseUrl= "http://localhost:5000";
-// export const baseUrl= "https://nextgen-kappa.vercel.app";
+// export const baseUrl= "http://localhost:5000";
+export const baseUrl= "https://nextgen-kappa.vercel.app";
 export const publicRequest=axios.create(
 {baseURL: baseUrl,
 headers:{
